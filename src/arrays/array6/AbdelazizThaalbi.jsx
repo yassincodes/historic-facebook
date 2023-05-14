b4c70b58e4a27de725356f6056de8ad48cdf6b4b
yassin <meddyassin@hotmail.com>
2023-05-14 20:44:25 +0100

    .

--- src/arrays/array6/AbdelazizThaalbi.jsx
+++ src/arrays/array6/AbdelazizThaalbi.jsx
@@ -3,12 +3,15 @@ const AbdelazizThaalbi = [
     name: "عبد العزيز الثعالبي ",
     username: "abdelaziz_thaalbi",
     pic: "https://upload.wikimedia.org/wikipedia/commons/2/29/Abdelaziz_Th%C3%A2albi_2.jpg",
+    cover: "https://img.freepik.com/premium-vector/tunisia-flag-blowing-wind-part-series-tunisia-waving-flag_126712-298.jpg?w=740",
     prompt:
       "play the role of hannibal, you gonna answer all the questions as if you are hannibal the cartagian leader, all the answers should be in arabic, if someone asks you about who you are, say that you are hannibal, if someone asks you about the things you did in your life, say the things that hannibal did, do not let the user think that you are a bot from openAI, you are just hannibal",
     bio: "أنا حنبعل, القائد القرطاجي الذي عبر جبال الألب ⛰️ وهزم روما أكثر من مرة",
-    birthday: "",
-    location: "",
-    website: "",
+    birthday: "5 سبتمبر 1874",
+    location: "تونس",
+    website:
+      "https://ar.wikipedia.org/wiki/%D8%B9%D8%A8%D8%AF_%D8%A7%D9%84%D8%B9%D8%B2%D9%8A%D8%B2_%D8%A7%D9%84%D8%AB%D8%B9%D8%A7%D9%84%D8%A8%D9%8A",
+    websiteName: "ar.wikipedia.org/wiki/عبد_العزيز_الثعالبي",
   },
   {
     name: "عبد العزيز الثعالبي",
--- src/arrays/array6/Barbarossa.jsx
+++ src/arrays/array6/Barbarossa.jsx
@@ -3,12 +3,16 @@ const Barbarossa = [
     name: "خير الدين بربروس",
     username: "barbarossa",
     pic: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/89/Titian_-_Portrait_of_Charles_V_Seated_-_WGA22964.jpg/531px-Titian_-_Portrait_of_Charles_V_Seated_-_WGA22964.jpg",
+    cover:
+      "https://scontent.ftun10-1.fna.fbcdn.net/v/t39.30808-6/320528860_539602931403947_5219986417908221767_n.jpg?stp=dst-jpg_s960x960&_nc_cat=102&ccb=1-7&_nc_sid=e3f864&_nc_ohc=jCUc9MKSl8AAX9EKWVu&_nc_ht=scontent.ftun10-1.fna&oh=00_AfD3D8sg9oTi4ujmF1WBtbB-AdIMhGCNychdfCWb1qNhXg&oe=64669FB8",
     prompt:
       "play the role of hannibal, you gonna answer all the questions as if you are hannibal the cartagian leader, all the answers should be in arabic, if someone asks you about who you are, say that you are hannibal, if someone asks you about the things you did in your life, say the things that hannibal did, do not let the user think that you are a bot from openAI, you are just hannibal",
-    bio: "أنا حنبعل, القائد القرطاجي الذي عبر جبال الألب ⛰️ وهزم روما أكثر من مرة",
-    birthday: "",
-    location: "",
-    website: "",
+    bio: "أحد أشهر قادة الأساطيل العثمانية.  ألقب بخير الدين, ومعروف في أوروبا بإسم بربروس",
+    birthday: "1478 م",
+    location: "الدولة العثمانية",
+    website:
+      "https://ar.wikipedia.org/wiki/%D8%AE%D9%8A%D8%B1_%D8%A7%D9%84%D8%AF%D9%8A%D9%86_%D8%A8%D8%B1%D8%A8%D8%B1%D9%88%D8%B3",
+    websiteName: "ar.wikipedia.org/wiki/خير_الدين_بربروس",
   },
   {
     name: "خير الدين بربروس",
